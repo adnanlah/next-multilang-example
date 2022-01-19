07a7f83e05ecd0fcbf73f036c2a71923702a1fc5
Adnan Lahrech <lahadn@gmail.com>
2022-01-19 02:18:54 +0100

    fetch allLocales instead

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,41 @@
 import Head from 'next/head';
 import Link from 'next/link'
 import { request } from "../lib/datocms";
-import styles from '../styles/App.module.css';
 
-export default function Home({englishArticles, frenchArticles, defaultArticles}) {
+export default function Home({allArticles}) {
   return (
     <div>
       <Head>
         <title>Multilanguage blog</title>
       </Head>
       <p>This is a <a href="https://nextjs.org/">Next.js</a> example of a multilanguage blog with translated slugs. Powered by <a href="https://www.datocms.com/">DatoCMS</a>.</p>
-      <b>Default locale</b>
+      <b>Default locale:</b>
       <ul>
-        {defaultArticles.map((article, idx) => {
+        {allArticles.map((article, idx) => {
           return (<li key={idx} >
-            <Link href={article.href} passHref><a>{article.name} <i>(#{article.id})</i></a></Link>
+            <Link href={`articles/${article._allSlugLocales.find(e => e.locale === 'en').value}`} passHref>
+              <a>{article._allNameLocales.find(e => e.locale === 'en').value}<i> (#{article.id})</i></a>
+            </Link>
           </li>)
         })}
       </ul>
-      <b>English locale</b>
+      <b>English locale:</b>
       <ul>
-        {englishArticles.map((article, idx) => {
+        {allArticles.map((article, idx) => {
           return (<li key={idx} >
-            <Link href={article.href} passHref><a>{article.name} <i>(#{article.id})</i></a></Link>
+            <Link href={`en/articles/${article._allSlugLocales.find(e => e.locale === 'en').value}`} passHref>
+              <a>{article._allNameLocales.find(e => e.locale === 'en').value}<i> (#{article.id})</i></a>
+            </Link>
           </li>)
         })}
       </ul>
-      <b>French locale</b>
+      <b>French locale:</b>
       <ul>
-        {frenchArticles.map((article, idx) => {
+        {allArticles.map((article, idx) => {
           return (<li key={idx} >
-            <Link href={article.href} passHref><a>{article.name} <i>(#{article.id})</i></a></Link>
+            <Link href={`fr/articles/${article._allSlugLocales.find(e => e.locale === 'fr').value}`} passHref>
+              <a>{article._allNameLocales.find(e => e.locale === 'fr').value}<i> (#{article.id})</i></a>
+            </Link>
           </li>)
         })}
       </ul>
@@ -38,15 +43,21 @@ export default function Home({englishArticles, frenchArticles, defaultArticles})
   )
 }
 
-export async function getServerSideProps(context) {
-  const dataEN = await request({query: `{allArticles(locale: en, orderBy: _createdAt_ASC) { id slug name }}`});
-  const dataFR = await request({query: `{allArticles(locale: fr, orderBy: _createdAt_ASC) { id slug name }}`});
+export async function getServerSideProps() {
+  const {allArticles} = await request({query: `{
+    allArticles(locale: en, orderBy: _createdAt_ASC) {
+      id
+      slug
+      _allNameLocales {
+        locale
+        value
+      }
+      _allSlugLocales {
+        locale
+        value
+      }
+    }
+  }`})
 
-  return {
-    props: {
-      defaultArticles: dataEN.allArticles.map((article) => ({...article, href: `articles/${article.slug}`})),
-      englishArticles: dataEN.allArticles.map((article) => ({...article, href: `en/articles/${article.slug}`})),
-      frenchArticles: dataFR.allArticles.map((article) => ({...article, href: `fr/articles/${article.slug}`})),
-    },
-  };
+  return { props: {allArticles} }
 }
